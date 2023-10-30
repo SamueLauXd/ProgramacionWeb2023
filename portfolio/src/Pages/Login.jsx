51112d7fc31ae4e52b85d51825adb1bc54c7e8ff
Sunart1 <79371501+Sunart1@users.noreply.github.com>
2023-10-29 20:21:05 -0500

    Login admin page

--- portfolio/src/Pages/Login.jsx
+++ portfolio/src/Pages/Login.jsx
@@ -1,35 +1,30 @@
-export function Login(params) {
+export function Login() {
     return (
-        <main className="grid grid-cols-2 px-20 py-10">
-            <div className="flex flex-col justify-center items-center">
+        <main className="grid grid-cols-2 px-20 py-20 h-[100vh]">
+            <div className="grid place-items-center">
                 <img src="./src/assets/icons/logo-large.svg" className="w-1/2" alt="U-Xperience Logo" />
-                <h1 className="font-raleway font-regular text-white text-lg">Administrator Panel</h1>
+                <h1 className="font-raleway font-regular text-white text-4xl w-min text-center">Administrator Panel</h1>
             </div>
-            <form>
-                <h5>Hello! <br /> Log in to enter the administrator panel</h5>
-                <div class="w-72">
-                    <div class="relative h-10 w-full min-w-[200px]">
-                        <input
-                            class="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                            placeholder=" "
-                        />
-                        <label class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-                            Username
-                        </label>
-                    </div>
-                </div>
-                <div class="w-72">
-                    <div class="relative h-10 w-full min-w-[200px]">
-                        <input
-                            class="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                            placeholder=" "
-                        />
-                        <label class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-                            Password
-                        </label>
-                    </div>
-                </div>
-            </form>
+            <LoginForm/>
         </main>
+    )
+}
+
+function LoginForm() {
+    return (
+        <form className="flex flex-col bg-[#1e2435] items-center justify-center border border-aquamarine-100 rounded-lg gap-12">
+            <h5 className="font-raleway text-white font-bold text-xl text-center">Hello! <br /> Log in to enter the administrator panel</h5>
+            <fieldset className="flex flex-col gap-12 w-1/2">
+                <div>
+                    <label for="username" class="font-lato block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                    <input type="text" id="username" class="font-lato bg-[#1e2435] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required />
+                </div>
+                <div>
+                    <label for="password" class="font-lato block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                    <input type="password" id="password" class="font-lato bg-[#1e2435] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" required />
+                </div>
+                <button className="bg-aquamarine-100 py-2 font-lato font-medium rounded-lg" type="submit">Sign In</button>
+            </fieldset>
+        </form>
     )
 }
