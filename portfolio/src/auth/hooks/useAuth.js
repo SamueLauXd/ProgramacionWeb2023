7c83c94306ff3539232b24e2fea200ebe87edb63
Sunart1 <79371501+Sunart1@users.noreply.github.com>
2023-11-29 10:58:37 -0500

    Change folder organization

--- portfolio/src/auth/hooks/useAuth.js
+++ portfolio/src/auth/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import { AuthContext } from "../auth/context/AuthContext"
+import { AuthContext } from "../context/AuthContext"
 
 export const useAuth = () => {
     const authContext = useContext(AuthContext)
